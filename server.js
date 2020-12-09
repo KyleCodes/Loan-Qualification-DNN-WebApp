5383f0e4be5ad2e23dd006c8c5637feb9f74692c
Haoguang Cai <12884255+unseenmars@users.noreply.github.com>
2020-12-08 23:53:14 -0800

    Update server.js

--- server.js
+++ server.js
@@ -135,9 +135,9 @@ app.get('/result', function (req, res) {
        console.log(parseInt(rowData.birthday, 10));
 
        let dataToSend;
-       const process = spawn('python3', ['./python/classification.py'], parseInt(rowData.gender, 10), parseInt(rowData.birthday, 10), parseInt(rowData.property, 10), parseInt(rowData.car, 10),
+       const process = spawn('python3', ['./python/classification.py', parseInt(rowData.gender, 10), parseInt(rowData.birthday, 10), parseInt(rowData.property, 10), parseInt(rowData.car, 10),
        parseInt(rowData.income, 10), parseInt(rowData.childnum, 10), rowData.eduLevel, rowData.incomeType, rowData.livingType, rowData.marital, 
-       parseInt(rowData.months, 10), parseInt(rowData.workPhone, 10), parseInt(rowData.Phone, 10), parseInt(rowData.email, 10), rowData.jobType);  // exec python as the shell commands
+       parseInt(rowData.months, 10), parseInt(rowData.workPhone, 10), parseInt(rowData.Phone, 10), parseInt(rowData.email, 10), rowData.jobType]);  // exec python as the shell commands
 
        process.stderr.on('data', function(data) {
          console.log(data.toString('utf8'))
